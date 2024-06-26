cce0028cb9e2f7c5bc7483c3d8c4c1c58789fbaa
Prasadini V.G.H. <prasadinivgh.21@uom.lk>
2024-06-27 00:59:27 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  const addNote = (text) =>{
+  const addNote = (text) => {
     //console.log(text);
     const date = new Date();
     const newNote = {
       id: nanoid(),
       text: text,
       date: date.toLocaleDateString(),
-    }
+    };
     const newNotes = [...notes, newNote];
     setNotes(newNotes);
-  }
+  };
 
-  const deleteNote = (id) =>{
-    const newNotes = notes.filter((note) => note.id!== id);
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-  }
+  };
 
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+      <Search handleSearchNote={setSearchText} />
+      <NotesList
+        notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
